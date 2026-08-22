48bb5ccacc2c87552b02786e71eba14e9b4a1205
PajauKat <mati.sabaku@gmail.com>
2026-08-22 09:54:10 -0400

    fix verify tipeo

--- api/auth/verify.js
+++ api/auth/verify.js
@@ -1,17 +1,19 @@
+import jwt from 'jsonwebtoken'
 
 export default async function handler(req, res) {
     const authHeaders = req.headers.authorization;
-    if(authHeaders || !authHeaders.startWith("Bearer ")){
-        return res.status(401).json({valid: false, error: "Falto el token o no parte con Bearer"})
+
+    // Se verifica que NO exista la cabecera O que NO empiece con "Bearer "
+    if (!authHeaders || !authHeaders.startsWith("Bearer ")) {
+        return res.status(401).json({ valid: false, error: "Faltó el token o no empieza con Bearer" });
     }
 
     const token = authHeaders.split(" ")[1];
 
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET)
-        return res.status(200).json({valid: true, userData: decoded})
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        return res.status(200).json({ valid: true, userData: decoded });
     } catch (err) {
-        return res.status(401).json({ valid: false, error: 'Token expirado o inválido' });
+        return res.status(401).json({ valid: false, error: "Token expirado o inválido" });
     }
-    
 }
